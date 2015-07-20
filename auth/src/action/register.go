c88faca3e86c08313a0754417e1188408c188fb0
root <root@localhost.localdomain>
2015-07-20 11:26:21 +0800

    modify

--- auth/src/action/register.go
+++ auth/src/action/register.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"time"
 )
 
 type (
@@ -15,15 +14,12 @@ type (
 		Acid        int
 		Ac_name     string
 		Ac_password string
-		Email       string
-		Mobile      string
 		Status      int
-		Create_time int
 	}
 )
 
 const (
-	INSERT string = "insert into account_tab (ac_name,ac_password,email,mobile,status,create_time) values (?,?,?,?,?,?)"
+	INSERT string = "insert into account_tab (ac_name,ac_password,status,create_time) values (?,?,?,unix_timestamp())"
 )
 
 func NewRegister() *Register {
@@ -33,15 +29,13 @@ func NewRegister() *Register {
 func (register *Register) Post(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	acname := req.FormValue("acname")
 	password := req.FormValue("password")
-	account := Account{Ac_name: acname, Ac_password: password,
-		Email: acname, Mobile: acname}
+	account := Account{Ac_name: acname, Ac_password: password}
 	ok := register_insert(&account)
 	if ok {
 		w.Write([]byte("0"))
 	} else {
 		w.Write([]byte("-1"))
 	}
-
 }
 
 func register_insert(ac *Account) (ok bool) {
@@ -56,6 +50,7 @@ func register_insert(ac *Account) (ok bool) {
 		fmt.Println(err)
 		return false
 	}
+
 	stmt, err := tx.Prepare(INSERT)
 	if err != nil {
 		fmt.Println(err)
@@ -63,8 +58,7 @@ func register_insert(ac *Account) (ok bool) {
 	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec(ac.Ac_name, ac.Ac_password, ac.Email, ac.Mobile, 0, time.Now().Unix())
-
+	_, err = stmt.Exec(ac.Ac_name, ac.Ac_password, 0)
 	if err != nil {
 		fmt.Println(err)
 		return false
