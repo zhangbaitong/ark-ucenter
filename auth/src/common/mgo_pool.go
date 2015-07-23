b71f994662fcc57b9ac46daee9fa8795cb78d6ca
root <root@localhost.localdomain>
2015-07-23 16:36:24 +0800

    add set user info

---
+++ auth/src/common/mgo_pool.go
@@ -0,0 +1,92 @@
+package common
+
+import (
+	"fmt"
+    "time"
+    "sync"
+	"errors"
+     "gopkg.in/mgo.v2" 
+)
+
+type MongoPool struct {
+    ServerName string
+    MaxPoolSize int
+    PoolSize int
+    Mu          sync.Mutex
+    Sessions       chan *mgo.Session
+}
+
+//建立Mongo接池，传入参数为：连接池最大连接数，服务名的连接数，是主库连接池还是备库连接池
+func CreateMongoPool(maxPoolsize int, strServerName string) *MongoPool {
+
+        dbPool := &MongoPool{MaxPoolSize: maxPoolsize, ServerName: strServerName}
+        flag := make(chan bool, dbPool.MaxPoolSize/2)
+        go func() {
+        for i := 0; i < dbPool.MaxPoolSize/2; i++ {
+            session, err := mgo.Dial(strServerName)
+            if err != nil {
+                fmt.Println(err)
+            }
+            dbPool.PutSession(session)
+            flag <- true
+        }
+        }()
+
+        for i := 0; i < dbPool.MaxPoolSize/2; i++ {
+        <-flag
+        }
+     dbPool.PoolSize= dbPool.MaxPoolSize/2
+
+        return dbPool
+}
+
+//从连接池中获取连接
+func (this *MongoPool) GetSession() (*mgo.Session, error) {
+    if this.PoolSize <this.MaxPoolSize && len(this.Sessions) == 0 {
+        go func() {
+            this.Mu.Lock()
+            if(this.PoolSize >=this.MaxPoolSize) {
+                return
+            }
+
+            for i := 0; i < this.MaxPoolSize/2; i++ {
+                session, err := mgo.Dial(this.ServerName)
+                if err != nil {
+                    fmt.Println("连接数据库失败")
+                    fmt.Println(err)
+                }
+                this.PutSession(session)
+            }
+            this.PoolSize=this.MaxPoolSize
+            this.Mu.Unlock()
+        }()
+    }
+
+    //判断是否能在3秒内获取连接，如果不能就报错
+    select {
+    //读取通道里的数据库连接，如果读不到就返回报错
+    case connChan, ok := <-this.Sessions:
+        {
+            if ok {
+                return connChan, nil
+            } else {
+                return nil, errors.New("数据库连接获取异常，可能已经被关闭！")
+            }
+        }
+    //如果被阻塞三秒仍没有获取到连接，则就返回错误
+    case <-time.After(time.Second * 3):
+        return nil, errors.New("获取数据库连接超时！")
+    }
+}
+
+//把连接放入连接池中
+func (this *MongoPool) PutSession(session *mgo.Session) {
+    if this.Sessions == nil {
+        this.Sessions = make(chan *mgo.Session, this.MaxPoolSize)
+    }
+    if len(this.Sessions) >= this.MaxPoolSize {
+        session.Close()
+        return
+    }
+    this.Sessions <- session
+}
--- auth/src/common/utils.go
+++ auth/src/common/utils.go
@@ -3,6 +3,7 @@ package common
 import (
 	"database/sql"
 	"fmt"
+	 "gopkg.in/mgo.v2" 
 	"github.com/garyburd/redigo/redis"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/zhangbaitong/go-uuid/uuid"
@@ -17,13 +18,14 @@ var (
 	Logger *log.Logger
 	pool   *redis.Pool
 	DBpool *DbPool
+	MPool *MongoPool
 )
 
 //Get db connection from mysql
 func GetDB() (db *sql.DB) {
 	if DBpool == nil {
-		DBpool = CreateDbPool(5, "mysql", "root:111111@tcp(117.78.19.76:3306)/at_db", true)
-		//DBpool = CreateDbPool(20, "mysql", "tomzhao:111111@tcp(127.0.0.1:3306)/at_db", true)
+		//DBpool = CreateDbPool(5, "mysql", "root:111111@tcp(117.78.19.76:3306)/at_db", true)
+		DBpool = CreateDbPool(20, "mysql", "tomzhao:111111@tcp(127.0.0.1:3306)/at_db", true)
 	}
 
 	conn, err := DBpool.GetConn()
@@ -47,6 +49,25 @@ func GetDBInfo() (info string) {
 	return fmt.Sprintf("PoolSize=%d;MaxPoolSize=%d", DBpool.PoolSize, DBpool.MaxPoolSize)
 }
 
+func GetSession() (Session *mgo.Session) {
+	if MPool == nil {
+		//DBpool = CreateDbPool(20, "mysql", "root:111111@tcp(117.78.19.76:3306)/at_db",true)
+		MPool = CreateMongoPool(20,"127.0.0.1:27017")
+	}
+
+	Session, err := MPool.GetSession()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return Session	
+}
+
+func FreeSession(Session *mgo.Session) {
+	MPool.PutSession(Session)
+}
+
 //get uuid lik a227cedf-e806-11e4-8666-3c075419d855
 func GetUID() string {
 	return uuid.NewUUID().String()
@@ -68,8 +89,8 @@ func GetRedisPool() *redis.Pool {
 			MaxIdle:     3,
 			IdleTimeout: 240 * time.Second,
 			Dial: func() (redis.Conn, error) {
-				c, err := redis.Dial("tcp", "117.78.19.76:6379")
-				//c, err := redis.Dial("tcp", "127.0.0.1:6379")
+				//c, err := redis.Dial("tcp", "117.78.19.76:6379")
+				c, err := redis.Dial("tcp", "127.0.0.1:6379")
 				if err != nil {
 					return nil, err
 				}
