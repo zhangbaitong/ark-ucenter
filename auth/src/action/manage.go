da2fdbf1beedf1cdd11d7f814c1176b159e61a4d
root <root@localhost.localdomain>
2015-09-07 10:19:14 +0800

    modified bugs

--- auth/src/action/manage.go
+++ auth/src/action/manage.go
@@ -7,9 +7,11 @@ import (
 	"strings"
 	"net/http"
 	"github.com/julienschmidt/httprouter"
+	"strconv" 
 )
 
 func GetOnlyCheckList(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Println("GetOnlyCheckList:")
 	FieldList:=GetCheckList()
 	strList:=""
 	for i := 0; i < len(FieldList); i++ {
@@ -24,8 +26,8 @@ func GetOnlyCheckList(w http.ResponseWriter, r *http.Request, _ httprouter.Param
 }
 
 func UpdateOnlyCheckList(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+
 	strFieldes := r.FormValue("fieldes")
-	fmt.Println("UpdateSearchFieldList:",strFieldes)
 	FieldList:=strings.Split(strFieldes, ",")
 
 	strBody := []byte("{\"Code\":0,\"Message\":\"ok\"}")	
@@ -36,3 +38,51 @@ func UpdateOnlyCheckList(w http.ResponseWriter, r *http.Request, _ httprouter.Pa
 	w.Write(strBody)
 }
 
+func PasswordReset(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	req.ParseForm()
+	fmt.Println(req.Form)
+
+	acname := req.FormValue("acname")
+	password := req.FormValue("password")
+	strBody:=[]byte("")
+	if acname == "" || password == "" {
+		strBody = []byte(fmt.Sprintf("{\"Code\":%d,\"Message\":\"%s\"}",PARAM_ERROR,GetError(PARAM_ERROR)))
+		w.Write(strBody)
+		return
+	}
+	//password=common.MD5(password)
+	ok := ResetPassword(acname,password)
+	if ok {
+		strBody = []byte("{\"Code\":0,\"Message\":\"ok\"}")
+	} else {
+		strBody = []byte(fmt.Sprintf("{\"Code\":%d,\"Message\":\"%s\"}",UPDATE_DB_ERROR,GetError(UPDATE_DB_ERROR)))
+	}
+	w.Write(strBody)
+}
+
+func ExportData(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	req.ParseForm()
+	fmt.Println(req.Form)
+
+	data_type := req.FormValue("data_type")
+	start_time := req.FormValue("start_time")
+	end_time := req.FormValue("end_time")
+	strBody:=[]byte("")
+	if data_type=="" || start_time == "" || end_time == "" {
+		strBody = []byte(fmt.Sprintf("{\"Code\":%d,\"Message\":\"%s\"}",PARAM_ERROR,GetError(PARAM_ERROR)))
+		w.Write(strBody)
+		return
+	}
+
+	StartTime, _ := strconv.Atoi(start_time)
+	EndTime, _ := strconv.Atoi(end_time)
+
+	//password=common.MD5(password)
+	user_count,ok := ExportMongo(StartTime,EndTime)
+	if ok {
+		strBody = []byte(fmt.Sprintf("{\"Code\":%d,\"Message\":\"user_count=%d\"}",OK,user_count))
+	} else {
+		strBody = []byte(fmt.Sprintf("{\"Code\":%d,\"Message\":\"%s\"}",UPDATE_DB_ERROR,GetError(UPDATE_DB_ERROR)))
+	}
+	w.Write(strBody)
+}
--- auth/src/example/http_client.go
+++ auth/src/example/http_client.go
@@ -6,8 +6,6 @@ import (
 	"common"
 	"gopkg.in/mgo.v2/bson" 
 	_"encoding/json"
-	_"time"
-	_"math/rand"
 	_"encoding/base64"
 )
 
@@ -21,45 +19,58 @@ type ATUserInfo struct {
 	Ac_id   int
 	Info map[string] string
 }
-const (
-SMS string="http://cloudsms.gyinfobird1.funzhou.cn/application/api?data=%s&interface_key=%s&interface_sign=%s"
-)
 
 func main() {
-/*
-	rand.Seed( time.Now().UTC().UnixNano())
-	code:=rand.Int()%1000000
-	//strMessage:=fmt.Sprintf("赵乐跃 VerifyCode:%06d",code)
-	strMessage:=fmt.Sprintf("赵乐跃 VerifyCode:%06d",code)
-	strTnterfaceKey:="ad79bd61-4cc8-f4a4-2811-55e0117e6cc4"
-	strInterfaceSign:="4bf38c7e184df4087910038afc7df8b9b899aa2f"
+/*	
+	var Url *url.URL
+	Url, err := url.Parse("http://sms.gyjbh.nvwayun.com")
+	if err != nil {
+	    panic("boom")
+	}
+
 	mesaage:=make(map[string]string)
 	mesaage["mobile"]="18585816540"
-	mesaage["msg"]=strMessage
+	mesaage["msg"]="888888(动态验证码),请在5分钟内输入该验证码."
 	strData, err := json.Marshal(mesaage)
 	if err != nil {
-		fmt.Println(err)
+		return
 	}
-	strSend:=base64.StdEncoding.EncodeToString(strData)
-	strSendURL:=fmt.Sprintf(SMS,strSend,strTnterfaceKey,strInterfaceSign)
 
-	strResult,err:=common.Invoker(common.HTTP_GET,strSendURL,"")
-	fmt.Println(strSendURL)
+	Url.Path += "/application/api"
+	strTnterfaceKey:="ad79bd61-4cc8-f4a4-2811-55e0117e6cc4"
+	strInterfaceSign:="4bf38c7e184df4087910038afc7df8b9b899aa2f"
+	strSend:=base64.StdEncoding.EncodeToString(strData)
+	parameters := url.Values{}
+	parameters.Add("data", strSend)
+	parameters.Add("interface_key", strTnterfaceKey)
+	parameters.Add("interface_sign", strInterfaceSign)
+	Url.RawQuery = parameters.Encode()
+	fmt.Printf("Encoded URL is %q\n", Url.String())
+
+	strResult,err:=common.Invoker(common.HTTP_GET,Url.String(),"")
+	if err!=nil {
+		return
+	}
+
+	fmt.Println(strResult)
+
+
+	//lue:=url.Values{"data_type":{"1"},"start_time": {"1439395200"},"end_time":{"1439740800"}}
+	value:=url.Values{"data_type":{"1"},"start_time": {"1441078590"},"end_time":{"1441078590"}}
+	strBody,err:=common.Invoker(common.HTTP_POST,"https://connect.funzhou.cn/manage/export_data",value)
 	if err!=nil {
 		fmt.Println(err)
 		return
 	}
-	result:=make(map[string]string)
-	json.Unmarshal([]byte(strResult),&result)
-	if result["result"]=="0" {
-		fmt.Println("Send SMS sucess!!")
-	} else {
-		fmt.Println("Send SMS faild because of ",result["result"])		
-	}
-	fmt.Println(strResult)
-*/
-	strVerifyCode:="306209"
-	value:=url.Values{"mobile": {"18585816540"},"verify_code":{strVerifyCode}}
+	fmt.Println(strBody)	
+
+
+	strVerifyCode:="912297" 
+	value:=url.Values{"mobile": {"18984898587"},"verify_code":{strVerifyCode}}
+	//value:=url.Values{"mobile": {"15519028660"},"verify_code":{strVerifyCode}}
+	//value:=url.Values{"mobile": {"18585816540"},"verify_code":{strVerifyCode}}
+	//value:=url.Values{"mobile": {"18984550575"},"verify_code":{strVerifyCode}}
+	//value:=url.Values{"mobile": {"18585816540"},"verify_code":{strVerifyCode}}
 	strBody,err:=common.Invoker(common.HTTP_POST,"https://connect.funzhou.cn/user/get_verify_code",value)
 	//strBody,err:=common.Invoker(common.HTTP_POST,"https://connect.funzhou.cn/user/check_verify_code",value)
 	if err!=nil {
@@ -67,11 +78,11 @@ func main() {
 		return
 	}
 	fmt.Println(strBody)	
-/*
-	//value:=url.Values{"acname": {"yanglinkang@qq.com "}}
+
+	value:=url.Values{"openid": {"55e170e1e138230f4zhw"}}
 	//value:=url.Values{"id": {"55cabc1de1382334ec000006"},"need_send_sms":{"0"}}
-	value:=url.Values{"id": {"55cabb3ae1382334ec000004"}}
-	//value:=url.Values{"email": {"zhw@sina.com"}}
+	//value:=url.Values{"id": {"55e14babe138232c79000001"}}
+	//value:=url.Values{"mobile": {"18585816666"}}
 	//value:=url.Values{"company": {"infobird1"}}
 	strBody,err:=common.Invoker(common.HTTP_POST,"https://connect.funzhou.cn/user/get_user_info",value)
 	if err!=nil {
@@ -83,7 +94,8 @@ func main() {
 	fmt.Println(result)	
 	fmt.Println(strBody)	
 
-	value:=url.Values{"reg_type":{"1"},"company_name": {"infobird"},"job":{"yanfa"},"company_addr":{""},"email":{""},"mobile":{""}}
+	password:=common.MD5("111111")
+	value:=url.Values{"acname":{"18984550000"},"password":{password},"name": {"zhanghuawen"},"company_name": {"infobird"},"job":{"yanfa"},"company_addr":{""},"email":{"18984550000@qq.com"},"mobile":{"18984550000"}}
 	strBody,err:=common.Invoker(common.HTTP_POST,"https://connect.funzhou.cn/user/register",value)
 	if err!=nil {
 		fmt.Println(err)
@@ -92,7 +104,7 @@ func main() {
 	fmt.Println(strBody)	
 
 	// "email": "zhw@sina.com",
-	value:=url.Values{"email": {""}}
+	value:=url.Values{"mobile": {"18984550575"}}
 	strBody,err:=common.Invoker(common.HTTP_POST,"https://connect.funzhou.cn/user/exist",value)
 	if err!=nil {
 		fmt.Println(err)
@@ -112,14 +124,24 @@ func main() {
 	}
 	fmt.Println(strBody)	
 
-	value:=url.Values{"acname": {"zhw"},"password":{"111111"},"email":{"zhw@sina.com"}}
+	value:=url.Values{"acname": {"123123123"},"password":{"111111"}}
 	strBody,err:=common.Invoker(common.HTTP_POST,"https://connect.funzhou.cn/user/register",value)
 	if err!=nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Println(strBody)	
+*/
 
+	value:=url.Values{"fieldes": {"qq,email,mobile,weibo,openid"}}
+	//strBody,err:=common.Invoker(common.HTTP_GET,"https://connect.funzhou.cn/manage/only_check_list",value)
+	strBody,err:=common.Invoker(common.HTTP_POST,"https://connect.funzhou.cn/manage/update_only_check_list",value)
+	if err!=nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(strBody)	
+/*
 	password:=common.MD5("33335")
 	value:=url.Values{"acname": {"muling@qq.com"},"password":{password}}
 	strBody,err:=common.Invoker(common.HTTP_POST,"https://connect.funzhou.cn/user/login",value)
@@ -132,9 +154,8 @@ func main() {
 	fmt.Println(result)	
 	fmt.Println(strBody)	
 
-	value:=url.Values{"id": {"55c9e63ae138231dac000011"},"name":{"张华文"}}
-	//strBody,err:=common.Invoker(common.HTTP_POST,"https://connect.funzhou.cn/user/set_user_info",value)
-	strBody,err:=common.Invoker(common.HTTP_POST,"http://127.0.0.1:8080/user/set_user_info",value)
+	value:=url.Values{"id": {"55e170e1e138230f48000004"},"name":{"张华文"},"mobile":{"18984550575"},"openid":{"55e170e1e138230f4zhw"}}
+	strBody,err:=common.Invoker(common.HTTP_POST,"https://connect.funzhou.cn/user/set_user_info",value)
 	if err!=nil {
 		fmt.Println(err)
 		return
@@ -143,8 +164,9 @@ func main() {
 	json.Unmarshal([]byte(strBody),&result)
 	fmt.Println(result)	
 	fmt.Println(strBody)	
-	
-	value:=url.Values{"id": {"55c0891de1382334bb000002"}}
+
+	//value:=url.Values{"id": {"55c0891de1382334bb000002"}}
+	value:=url.Values{"mobile": {"18984550575"}}
 	strBody,err:=common.Invoker(common.HTTP_POST,"https://connect.funzhou.cn/user/get_user_info",value)
 	if err!=nil {
 		fmt.Println(err)
